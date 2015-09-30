acc3fa2b10129cbcb9e5de43046f4cf76b5d024d
Paul Le Cam <paul@ulem.net>
2015-09-30 08:28:09 +0100

    Add webpack

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+/* eslint-disable */
+module.exports = {
+  output: {
+    library: 'ReactLeaflet',
+    libraryTarget: 'umd'
+  },
+  externals: [
+    {
+      leaflet: {
+        root: 'L',
+        commonjs2: 'leaflet',
+        commonjs: 'leaflet',
+        amd: 'leaflet'
+      },
+      react: {
+        root: 'React',
+        commonjs2: 'react',
+        commonjs: 'react',
+        amd: 'react'
+      }
+    }
+  ],
+  module: {
+    loaders: [
+      {test: /\.js$/, exclude: /node_modules/, loader: 'babel'}
+    ]
+  }
+};
