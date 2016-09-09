1f57b3c217fe912d04f13b26c858b471ab953a69
John de Stigter <rjdestigter@gmail.com>
2016-09-09 15:43:35 -0600

    Added pane context to ImageOverlay.

--- src/ImageOverlay.js
+++ src/ImageOverlay.js
@@ -4,6 +4,7 @@ import { imageOverlay } from 'leaflet'
 import { PropTypes } from 'react'
 
 import boundsType from './types/bounds'
+import paneType from './types/pane'
 import MapLayer from './MapLayer'
 
 export default class ImageOverlay extends MapLayer {
@@ -14,10 +15,18 @@ export default class ImageOverlay extends MapLayer {
     url: PropTypes.string.isRequired,
   };
 
+  static contextTypes = {
+    ...(MapLayer.contextTypes || {}),
+    pane: paneType,
+  };
+
   componentWillMount () {
     super.componentWillMount()
     const { bounds, url, ...props } = this.props
-    this.leafletElement = imageOverlay(url, bounds, props)
+    this.leafletElement = imageOverlay(url, bounds, {
+      ...props,
+      pane: this.context.pane,
+    })
   }
 
   componentDidUpdate (prevProps: Object) {
