9e3c417c3d507c3c13e3985109e7df0354878358
Dustin L. Dodson <dustinleedodson@gmail.com>
2018-08-30 19:42:38 -0800

    Fixed attributions not being updated when props change.

--- src/TileLayer.js
+++ src/TileLayer.js
@@ -18,7 +18,14 @@ class TileLayer extends GridLayer<LeafletElement, Props> {
     super.updateLeafletElement(fromProps, toProps)
     if (toProps.url !== fromProps.url) {
       this.leafletElement.setUrl(toProps.url)
-    }
+	}
+
+	if(toProps.attribution !== fromProps.attribution) {
+		const map = toProps.leaflet.map;
+
+		map.attributionControl.removeAttribution(fromProps.attribution);
+		map.attributionControl.addAttribution(toProps.attribution);
+	}
   }
 }
 
