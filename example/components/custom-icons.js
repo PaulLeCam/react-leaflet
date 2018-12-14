416688deb21e7071d1de48a009ba565476ee08f1
Rob Lahoda <rob@roblahoda.com>
2018-12-14 10:40:01 -0500

    Refactored to a single component with the icon declarations and map/marker together for greater clarity and simplicity for the example

--- example/components/custom-icons.js
+++ example/components/custom-icons.js
@@ -2,13 +2,35 @@
 
 import React, { Component } from 'react'
 import { Map, TileLayer, Marker, Popup } from '../../src'
-import { pointerIcon, suitcasePoint } from './icons'
+import L from 'leaflet'
 
 type State = {
   lat: number,
   lng: number,
   zoom: number,
 }
+
+export const pointerIcon = new L.Icon({
+  iconUrl: require('../assets/pointerIcon.svg'),
+  iconRetinaUrl: require('../assets/pointerIcon.svg'),
+  iconAnchor: [5, 55],
+  popupAnchor: [10, -44],
+  iconSize: [25, 55],
+  shadowUrl: '../assets/marker-shadow.png',
+  shadowSize: [68, 95],
+  shadowAnchor: [20, 92],
+})
+
+export const suitcasePoint = new L.Icon({
+  iconUrl: require('../assets/suitcaseIcon.svg'),
+  iconRetinaUrl: require('../assets/suitcaseIcon.svg'),
+  iconAnchor: [20, 40],
+  popupAnchor: [0, -35],
+  iconSize: [40, 40],
+  shadowUrl: '../assets/marker-shadow.png',
+  shadowSize: [29, 40],
+  shadowAnchor: [7, 40],
+})
 
 export default class CustomIcons extends Component<{}, State> {
   state = {
