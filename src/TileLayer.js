fef6c6b0e45ae8b1833854c18419073d831a9e82
Giovanni Montenegro <giovanni.montenegro@aurigaspa.com>
2019-04-15 10:56:08 +0200

    TileLayer customizable create tile

--- src/TileLayer.js
+++ src/TileLayer.js
@@ -1,25 +1,36 @@
 // @flow
+import { TileLayer as LeafletTileLayer } from 'leaflet';
 
-import { TileLayer as LeafletTileLayer } from 'leaflet'
+import { withLeaflet } from './context';
+import GridLayer from './GridLayer';
 
-import { withLeaflet } from './context'
-import GridLayer from './GridLayer'
-import type { GridLayerProps } from './types'
+import type { GridLayerProps } from './types';
 
-type LeafletElement = LeafletTileLayer
-type Props = { url: string } & GridLayerProps
+type LeafletElement = LeafletTileLayer;
+type coords = { z: string, x: string, y: string };
+type Props = {
+	url: string,
+	create?: (coords: coords, done: Promise<any>) => LeafletElement,
+} & GridLayerProps;
 
 class TileLayer extends GridLayer<LeafletElement, Props> {
-  createLeafletElement(props: Props): LeafletElement {
-    return new LeafletTileLayer(props.url, this.getOptions(props))
-  }
+	createLeafletElement(props: Props): LeafletElement {
+		const leafletTile: LeafletElement = new LeafletTileLayer(
+			props.url,
+			this.getOptions(props),
+		);
+		if (props.create) {
+			leafletTile.createTile = props.create;
+		}
+		return leafletTile;
+	}
 
-  updateLeafletElement(fromProps: Props, toProps: Props) {
-    super.updateLeafletElement(fromProps, toProps)
-    if (toProps.url !== fromProps.url) {
-      this.leafletElement.setUrl(toProps.url)
-    }
-  }
+	updateLeafletElement(fromProps: Props, toProps: Props) {
+		super.updateLeafletElement(fromProps, toProps);
+		if (toProps.url !== fromProps.url) {
+			this.leafletElement.setUrl(toProps.url);
+		}
+	}
 }
 
-export default withLeaflet<Props, TileLayer>(TileLayer)
+export default withLeaflet<Props, TileLayer>(TileLayer);
