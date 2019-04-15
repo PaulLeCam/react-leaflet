397882ccca314ec987e91fd5e2f7fa4bffcadd40
Giovanni Montenegro <giovanni.montenegro@aurigaspa.com>
2019-04-15 12:54:36 +0200

    Push a better code format

--- src/TileLayer.js
+++ src/TileLayer.js
@@ -1,36 +1,36 @@
 // @flow
-import { TileLayer as LeafletTileLayer } from 'leaflet';
+import { TileLayer as LeafletTileLayer } from 'leaflet'
 
-import { withLeaflet } from './context';
-import GridLayer from './GridLayer';
+import { withLeaflet } from './context'
+import GridLayer from './GridLayer'
 
-import type { GridLayerProps } from './types';
+import type { GridLayerProps } from './types'
 
-type LeafletElement = LeafletTileLayer;
-type coords = { z: string, x: string, y: string };
+type LeafletElement = LeafletTileLayer
+type coords = { z: string, x: string, y: string }
 type Props = {
-	url: string,
-	create?: (coords: coords, done: Promise<any>) => LeafletElement,
-} & GridLayerProps;
+  url: string,
+  create?: (coords: coords, done: Promise<any>) => LeafletElement
+} & GridLayerProps
 
 class TileLayer extends GridLayer<LeafletElement, Props> {
-	createLeafletElement(props: Props): LeafletElement {
-		const leafletTile: LeafletElement = new LeafletTileLayer(
-			props.url,
-			this.getOptions(props),
-		);
-		if (props.create) {
-			leafletTile.createTile = props.create;
-		}
-		return leafletTile;
-	}
+  createLeafletElement(props: Props): LeafletElement {
+    const leafletTile: LeafletElement = new LeafletTileLayer(
+      props.url,
+      this.getOptions(props)
+    )
+    if (props.create) {
+      leafletTile.createTile = props.create
+    }
+    return leafletTile
+  }
 
-	updateLeafletElement(fromProps: Props, toProps: Props) {
-		super.updateLeafletElement(fromProps, toProps);
-		if (toProps.url !== fromProps.url) {
-			this.leafletElement.setUrl(toProps.url);
-		}
-	}
+  updateLeafletElement(fromProps: Props, toProps: Props) {
+    super.updateLeafletElement(fromProps, toProps)
+    if (toProps.url !== fromProps.url) {
+      this.leafletElement.setUrl(toProps.url)
+    }
+  }
 }
 
-export default withLeaflet<Props, TileLayer>(TileLayer);
+export default withLeaflet<Props, TileLayer>(TileLayer)
