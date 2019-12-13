88db6a509bbd6b2f91ef873a1fd87338d4185aa0
C. Warren Dale <dale@cwarrendale.com>
2019-12-13 10:02:41 -0600

    typeof

--- src/utils/pick.js
+++ src/utils/pick.js
@@ -2,7 +2,7 @@
 
 export default function pick(object: Object, keys: Array<string>): Object {
   return keys.reduce((obj, key) => {
-    if (object[key] !== undefined) {
+    if (typeof object[key] !== 'undefined') {
       obj[key] = object[key]
     }
     return obj
