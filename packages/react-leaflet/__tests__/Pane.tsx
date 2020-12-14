938dc8d83aedc37bbd4860378b079e15d2bf8857
Zeb Burke-Conte <zebburkeconte@gmail.com>
2020-12-13 20:37:04 -0800

    Add failing test: rendering Pane in strict mode

---
+++ packages/react-leaflet/__tests__/Pane.tsx
@@ -0,0 +1,44 @@
+import { render } from '@testing-library/react'
+import React from 'react'
+
+import { MapContainer, Pane, TileLayer } from '../src'
+
+describe('Pane', () => {
+  test('renders', () => {
+    const { container } = render(
+      <MapContainer center={[0, 0]} zoom={10}>
+        <Pane style={{zIndex: 1000}}>
+          <TileLayer attribution="tiles attribution" url="http://localhost" />
+        </Pane>
+      </MapContainer>,
+    )
+    expect(container).toMatchSnapshot()
+  })
+
+  test('renders with a name', () => {
+    const { container } = render(
+      <MapContainer center={[0, 0]} zoom={10}>
+        <Pane name="foo" style={{zIndex: 1000}}>
+          <TileLayer attribution="tiles attribution" url="http://localhost" />
+        </Pane>
+      </MapContainer>,
+    )
+    expect(container).toMatchSnapshot()
+  })
+
+  test('renders in Strict Mode', () => {
+    const { container } = render(
+      <React.StrictMode>
+        <MapContainer center={[0, 0]} zoom={10}>
+          <Pane style={{zIndex: 1000}}>
+            <TileLayer attribution="tiles attribution" url="http://localhost" />
+          </Pane>
+          <Pane name="foo" style={{zIndex: 1001}}>
+            <TileLayer attribution="tiles attribution" url="http://localhost" />
+          </Pane>
+        </MapContainer>
+      </React.StrictMode>,
+    )
+    expect(container).toMatchSnapshot()
+  })
+})
