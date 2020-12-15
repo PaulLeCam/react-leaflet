0f3d929e96e8c63438d2ef5e85ecc50edc49e957
Zeb Burke-Conte <zebburkeconte@gmail.com>
2020-12-14 23:18:52 -0800

    Test nested panes, unmounting and remounting

--- packages/react-leaflet/__tests__/Pane.tsx
+++ packages/react-leaflet/__tests__/Pane.tsx
@@ -27,4 +27,52 @@ describe('Pane', () => {
     )
     expect(container).toMatchSnapshot()
   })
+
+  test('renders nested panes', () => {
+    const { container } = render(
+      <React.StrictMode>
+        <MapContainer center={[0, 0]} zoom={10}>
+          <Pane name="foo" style={{ zIndex: 1001 }}>
+            <Pane name="bar" style={{ zIndex: 4000 }}>
+              <TileLayer attribution="tiles attribution" url="http://localhost/nested" />
+            </Pane>
+            <TileLayer attribution="tiles attribution" url="http://localhost/not-nested" />
+          </Pane>
+        </MapContainer>
+      </React.StrictMode>,
+    )
+    expect(container).toMatchSnapshot()
+  })
+
+  test('can unmount and remount with the same name', () => {
+    const { rerender, container } = render(
+      <MapContainer center={[0, 0]} zoom={10}>
+        <Pane name="foo" style={{ zIndex: 1001 }}>
+          <Pane name="bar" style={{ zIndex: 4000 }}>
+            <TileLayer attribution="tiles attribution" url="http://localhost/nested" />
+          </Pane>
+          <TileLayer attribution="tiles attribution" url="http://localhost/not-nested" />
+        </Pane>
+      </MapContainer>,
+    )
+    expect(container).toMatchSnapshot()
+
+    rerender(
+      <MapContainer center={[0, 0]} zoom={10}>
+      </MapContainer>,
+    )
+    expect(container).toMatchSnapshot()
+
+    rerender(
+      <MapContainer center={[0, 0]} zoom={10}>
+        <Pane name="bar" style={{ zIndex: 1001 }}>
+          <Pane name="foo" style={{ zIndex: 4000 }}>
+            <TileLayer attribution="tiles attribution" url="http://localhost/new-foo-nested" />
+          </Pane>
+          <TileLayer attribution="tiles attribution" url="http://localhost/new-bar-not-nested" />
+        </Pane>
+      </MapContainer>
+    )
+    expect(container).toMatchSnapshot()
+  })
 })
